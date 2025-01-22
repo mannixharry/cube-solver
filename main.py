bf49278ae1a61df25167e3e84f3abeaae6d6d0b8
mannixharry <142188929+mannixharry@users.noreply.github.com>
2025-01-22 11:58:58 +0000

    added mouse contol of cube

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import solver
 import pygame
 from pygame.locals import * 
 import cube 
-
+import vision
 def generate_solve_moves(full_solution):
     return (i for i in full_solution)
 
 def main(cube_string='WWWWWWWWWGGGGGGGGGOOOOOOOOORRRRRRRRRBBBBBBBBBYYYYYYYYY'):
 
+    cube_capturer = vision.Capturer()
     cube_manager = CubeManager(cube_string)
     start_solve_demonstration = False
 
@@ -48,7 +49,7 @@ def main(cube_string='WWWWWWWWWGGGGGGGGGOOOOOOOOORRRRRRRRRBBBBBBBBBYYYYYYYYY'):
             cube_manager.set_cube_view(Move.F)
 
         if keys[K_SPACE]:
-            if not start_solve_demonstration:
+            if not start_solve_demonstration and not cube_manager.face_turning:
                 start_solve_demonstration = True
                 cube_solver = solver.Solver()
                 print(cube_manager.cube)  # Current cube state
@@ -66,12 +67,16 @@ def main(cube_string='WWWWWWWWWGGGGGGGGGOOOOOOOOORRRRRRRRRBBBBBBBBBYYYYYYYYY'):
                     start_solve_demonstration = False
 
         if keys[K_TAB]:
-            new_cube = cube_manager.cube
-            main(str(cube.FaceletCube(new_cube)))
-            running = False
+            captured_cube =  cube_capturer.capture_cube()
+            start_solve_demonstration = False 
+
+            if captured_cube: 
+                cube_manager = CubeManager(captured_cube)
+
+        if keys[K_RETURN]:
+            cube_manager = CubeManager(cube.CubieCube())
 # Demonstration with alternating actions
         if start_solve_demonstration:
-        
             current_time = pygame.time.get_ticks()
             if current_time - last_move_time >= move_interval_ms:
                 try:
@@ -114,16 +119,32 @@ def main(cube_string='WWWWWWWWWGGGGGGGGGOOOOOOOOORRRRRRRRRBBBBBBBBBYYYYYYYYY'):
         face_key_list = [K_u, K_f, K_l, K_r, K_b, K_d]
         
         if not start_solve_demonstration:
-            move_number = next((i for i, val in enumerate(face_key_list) if keys[val]), None)
-            if move_number is not None:
-                move = Move(move_number)
-                if keys[K_LSHIFT]:
-                    move += 12  # Counter-clockwise
-                if keys[K_LCTRL]:
-                    move += 6  # Double move
+            
+            clicked_facelet = cube_manager.get_clicked_facelet()
+            if clicked_facelet != None:
+                print(clicked_facelet//9)
+                move = Move(clicked_facelet // 9)
+                counter_clockwise = (clicked_facelet % 9) in [0,3,6]
+                double = (clicked_facelet%9) in [1,4,7]
+                if counter_clockwise:
+                    move += 12 
+                if double:
+                    move+=6
+                
+                cube_manager.set_face_turn(move)
+                
+            else:
+                move_number = next((i for i, val in enumerate(face_key_list) if keys[val]), None)
+            
+                if move_number is not None:
+                    move = Move(move_number)
+                    if keys[K_LSHIFT]:
+                        move += 12  # Counter-clockwise
+                    if keys[K_LCTRL]:
+                        move += 6  # Double move
                     
             
-                cube_manager.set_face_turn(move)
+                    cube_manager.set_face_turn(move)
 
         cube_manager.main()
         fps = clock.get_fps()
